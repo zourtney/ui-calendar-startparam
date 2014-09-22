58ba36bedf2b2f51bf4be33ffb2aa48251403c35
Courtney Christensen <zourtney@gmail.com>
2014-09-22 09:10:21 -0700

    Add event on current day instead

--- app/app.js
+++ app/app.js
@@ -2,13 +2,8 @@
 
 angular.module('calendar', ['ui.calendar']).controller('MainCtrl', ['$scope', function($scope) {
   
-  var date = new Date();
-  var d = date.getDate();
-  var m = date.getMonth();
-  var y = date.getFullYear();
-
   $scope.eventSources = [
-    [ {title: 'First of month happenings', startDate: new Date(y, m, 1)} ]
+    [ {title: 'It\'s today!', startDate: new Date()} ]
   ];
 
   $scope.calendarConfig = {
